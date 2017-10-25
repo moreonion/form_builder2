34661c50211e1f10c892dd998403145a4d4fdb05
Onur D <contact@dinony.com>
2017-10-25 13:22:17 +0200

    Create component files

--- src/FormBuilder.vue
+++ src/FormBuilder.vue
@@ -1,13 +1,23 @@
 <template>
   <el-row :gutter="20">
-    <el-col :xs="24" :sm="8">Palette</el-col>
-    <el-col :xs="24" :sm="16">Builder</el-col>
+    <el-col :xs="24" :sm="8">
+      <mo-palette></mo-palette>
+    </el-col>
+    <el-col :xs="24" :sm="16">
+      <mo-builder></mo-builder>
+    </el-col>
   </el-row>
 </template>
 
 <script>
+import Palette from './components/Palette.vue'
+import Builder from './components/Builder.vue'
+
 export default {
-  name: 'mo-form-builder',
+  components: {
+    'mo-palette': Palette,
+    'mo-builder': Builder
+  },
   data () {
     return {
     }
---
+++ src/components/Palette.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Palette</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
