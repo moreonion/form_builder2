e2d925b3259a475112d0123f735c0d1ca7190d37
Onur D <contact@dinony.com>
2017-11-30 18:10:00 +0100

    Simplify palette template

--- src/components/Palette.vue
+++ src/components/Palette.vue
@@ -1,60 +1,31 @@
 <template>
   <el-collapse v-model="activeName" accordion>
-    <el-collapse-item title="Contact record fields" name="1">
-      <el-form id="contactFields" :model="form" ref="contactFields">
-        <el-form-item label="First name">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Last name">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Salutation">
-          <el-input></el-input>
-        </el-form-item>
-      </el-form>
-    </el-collapse-item>
-    <el-collapse-item title="General fields" name="2">
-      <el-form id="generalFields" ref="generalFields">
-        <el-form-item label="Last name">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Salutation">
-          <el-input></el-input>
-        </el-form-item>
-      </el-form>
-    </el-collapse-item>
-    <el-collapse-item title="Payment fields" name="3">
-      <el-form id="paymentFields" :model="form" ref="paymentFields">
-        <el-form-item label="Salutation">
-          <el-input></el-input>
-        </el-form-item>
-      </el-form>
+    <el-collapse-item
+      :key="i" v-for="(paletteGroup, i) in paletteConfig.groups"
+      :title="paletteGroup.label" :name="i" >
+      <div class="paletteItem" :key="j" v-for="(field, j) in paletteGroup.fields">
+        {{field.label}}
+      </div>
     </el-collapse-item>
   </el-collapse>
 </template>
 
 <script>
 export default {
+  props: ['paletteConfig'],
   data () {
     return {
-      activeName: '1',
-      form: {
-        firstName: null
-      },
-      items: [
-        'First Name',
-        'Second Name'
-      ]
-    }
-  },
-  mounted: function() {
-    for(let ref in this.$refs) {
-      this.$emit('append', 'left', this.$refs[ref].$el)
+      activeName: 0,
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+  .paletteItem {
+    padding: 5px;
+    margin-bottom: 5px;
+    border: 1px solid grey;
+    border-radius: 2px;
+  }
 </style>
