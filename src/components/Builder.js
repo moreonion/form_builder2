3e6009b054d75c77665aef4442a5ada57219130d
Onur D <contact@dinony.com>
2018-01-02 15:54:24 +0100

    Add root node component

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,8 +1,9 @@
 export default {
   functional: true,
-  render: (h, context) => {
-    return context.props.rootNode.children.map(c => {
-      return <mo-node node={c}></mo-node>
+  render(h, context) {
+    // Children of root node must be 'page' nodes
+    return context.props.rootNode.children.map(child => {
+      return child.renderNode(h)
     })
   }
 }
