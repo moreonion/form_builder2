ec8ce05573242e5561624418bbaa1e0892e43277
Onur D <contact@dinony.com>
2018-01-04 14:39:38 +0100

    Add paletteItem getter

--- src/components/palette/store.js
+++ src/components/palette/store.js
@@ -6,5 +6,10 @@ export const paletteModule = {
   namespaced: true,
   state: {
     paletteConfig
+  },
+  getters: {
+    getPaletteItem: state => (groupIndex, itemIndex) => {
+      return state.paletteConfig.groups[groupIndex].fields[itemIndex]
+    }
   }
 }
