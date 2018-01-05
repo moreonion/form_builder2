4e08172b1110d3de4ccbb95deddfcb542eb9cbeb
Onur D <contact@dinony.com>
2018-01-05 11:45:24 +0100

    Rm global palette dnd options

--- src/components/palette/store.js
+++ src/components/palette/store.js
@@ -1,11 +1,9 @@
 import {getPaletteConfig} from './initState'
-import {PALETTE_DND_OPTIONS} from '../../config/dnd'
 
 export const paletteModule = {
   namespaced: true,
   state: {
     paletteConfig: getPaletteConfig(),
-    dndOptions: PALETTE_DND_OPTIONS,
     activeName: 1 // active collapse index
   },
   getters: {
