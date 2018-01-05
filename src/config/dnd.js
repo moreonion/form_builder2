9f988fb682dd9b65015e5a2bbfa8d120f18fcf5b
Onur D <contact@dinony.com>
2018-01-05 13:23:25 +0100

    Allow putting into builder group

--- src/config/dnd.js
+++ src/config/dnd.js
@@ -2,6 +2,6 @@ export const PALETTE_DND_GROUP = 'palette'
 export const BUILDER_DND_GROUP = 'tree'
 
 export const BUILDER_DND_OPTIONS = {
-  group: {name: BUILDER_DND_GROUP, pull: true, put: [PALETTE_DND_GROUP]},
+  group: {name: BUILDER_DND_GROUP, pull: true, put: [BUILDER_DND_GROUP, PALETTE_DND_GROUP]},
   animation: 100
 }
