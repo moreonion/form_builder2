2686483a4568a4724fd2493eeaa6668d2db889ba
Onur D <contact@dinony.com>
2018-01-05 12:43:53 +0100

    Adapt page node

--- src/components/builder/nodes/general/page.js
+++ src/components/builder/nodes/general/page.js
@@ -1,7 +1,18 @@
-import DnDNode from '../base/dnd'
+import IntermediateNode from '../base/intermediate'
 
-export default class PageNode extends DnDNode {
+export default class PageNode extends IntermediateNode {
   constructor(initChildren=[], dndOptions=undefined) {
     super(initChildren, dndOptions)
+  }
+
+  renderNode(h) {
+    return (
+      <div style={{border: '10px solid pink'}}>
+        <h1>Page</h1>
+        <div style={{border: '5px dashed pink', margin: '5px'}}>
+          {this.children.map(child => child.renderNode(h))}
+        </div>
+      </div>
+    )
   }
 }
