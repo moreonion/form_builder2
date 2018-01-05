c3db7e0c991c988b9d32fd1d9856db2ef15617a7
Onur D <contact@dinony.com>
2018-01-05 15:33:49 +0100

    Add method to encode node path in tree

--- src/components/builder/init-state.js
+++ src/components/builder/init-state.js
@@ -2,6 +2,7 @@ import IntermediateNode from './nodes/base/intermediate'
 import DnDNode from './nodes/base/dnd'
 import PageNode from './nodes/general/page'
 import TextFieldNode from './nodes/general/text'
+import {encodePath} from './encode-path'
 
 const initState = new IntermediateNode([ // --> root node
   new DnDNode([
@@ -16,5 +17,13 @@ const initState = new IntermediateNode([ // --> root node
     ])
   ])
 ])
+
+/*
+ * Attach path encoding
+ * E.g.: 0-1-0-2
+ * Meaning start with root node, take 0 child, then 1 child, ... to locate leaf
+ * Needed to manage data model with nested drag and drop.
+ */
+encodePath(initState, '')
 
 export default initState
