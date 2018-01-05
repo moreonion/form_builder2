bbe86bd88fd0a0aee7976defdf513dca8d4157e5
Onur D <contact@dinony.com>
2018-01-05 09:44:48 +0100

    better formatting

--- src/components/builder/initState.js
+++ src/components/builder/initState.js
@@ -2,6 +2,11 @@ import IntermediateNode from './nodes/base/intermediate'
 import PageNode from './nodes/general/page'
 import TextFieldNode from './nodes/general/textfield'
 
-const initState = new IntermediateNode([new PageNode([new TextFieldNode('a'), new TextFieldNode('b')])])
+const initState = new IntermediateNode([
+  new PageNode([
+    new TextFieldNode('a'),
+    new TextFieldNode('b')
+  ])
+])
 
 export default initState
