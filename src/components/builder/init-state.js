0a6c3228489858e13ef3fd24be22453d88a9a60d
Onur D <contact@dinony.com>
2018-01-05 14:42:19 +0100

    Tmp adapt init state

--- src/components/builder/init-state.js
+++ src/components/builder/init-state.js
@@ -6,6 +6,9 @@ import TextFieldNode from './nodes/general/text'
 const initState = new IntermediateNode([ // --> root node
   new DnDNode([
     new PageNode([
+      new DnDNode([])
+    ]),
+    new PageNode([
       new DnDNode([
         new TextFieldNode('a'),
         new TextFieldNode('b')
