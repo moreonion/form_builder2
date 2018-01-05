76f3bae691c993f9a55f27eea74c9ada09d3f17c
Onur D <contact@dinony.com>
2018-01-05 12:44:07 +0100

    Adapt builder init state

--- src/components/builder/initState.js
+++ src/components/builder/initState.js
@@ -1,11 +1,14 @@
 import IntermediateNode from './nodes/base/intermediate'
+import DnDNode from './nodes/base/dnd'
 import PageNode from './nodes/general/page'
 import TextFieldNode from './nodes/general/textfield'
 
 const initState = new IntermediateNode([
   new PageNode([
-    new TextFieldNode('a'),
-    new TextFieldNode('b')
+    new DnDNode([
+      new TextFieldNode('a'),
+      new TextFieldNode('b')
+    ])
   ])
 ])
 
