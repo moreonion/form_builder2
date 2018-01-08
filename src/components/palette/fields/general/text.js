d2b2ad3209682a676e46f61bf31bc964a1c0e5ac
Onur D <contact@dinony.com>
2018-01-08 16:25:02 +0100

    Fix class name

--- src/components/palette/fields/general/text.js
+++ src/components/palette/fields/general/text.js
@@ -1,5 +1,5 @@
 import AbstractField from '../base/abstract'
-import TextfieldNode from '../../../builder/nodes/general/text'
+import TextFieldNode from '../../../builder/nodes/general/text'
 
 export default class PaletteTextField extends AbstractField {
   constructor(label='Textfield', icon='fa fa-text-width') {
@@ -7,6 +7,6 @@ export default class PaletteTextField extends AbstractField {
   }
 
   nodeFactory() {
-    return new TextfieldNode('New textfield')
+    return new TextFieldNode('New textfield')
   }
 }
