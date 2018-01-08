887712945f56788bd79f4fd3340769751f92e1f0
Onur D <contact@dinony.com>
2018-01-08 18:11:58 +0100

    Add mutations for palette config state

--- src/components/palette/store.js
+++ src/components/palette/store.js
@@ -1,23 +1,49 @@
 import {getInitPaletteState} from './init-state'
 
+import {NODE_TYPE_FIRST_NAME} from '../builder/nodes/contact/first-name'
+import {NODE_TYPE_LAST_NAME} from '../builder/nodes/contact/last-name'
+
+import {
+  BUILDER_SINGLETON_NODES,
+  BUILDER_IS_SINGLETON_NODE_MAP
+} from '../../config/builder'
+
 export const paletteModule = {
   namespaced: true,
   state: {
     palette: getInitPaletteState(),
     paletteConfig: {
-      createdSingletons: {
-        firstName: false,
-        lastName: false
-      }
+      createdSingletons: BUILDER_SINGLETON_NODES.reduce((accum, nodeType) => {
+        accum[nodeType] = false
+        return accum
+      }, {})
     },
     activeName: 0 // active collapse index
   },
   getters: {
-    paletteState: ({palette, paletteConfig}) => {
-      return palette
+    paletteState: ({palette, paletteConfig:{createdSingletons}}) => {
+      return {
+        groups: palette.groups.map(group => {
+          return {
+            ...group,
+            fields: group.fields.filter(item => {
+              return !BUILDER_IS_SINGLETON_NODE_MAP[item.nodeType]
+                || !createdSingletons[item.nodeType]
+            })
+          }
+        })
+      }
     },
-    getPaletteItem: state => (groupIndex, itemIndex) => {
-      return state.palette.groups[groupIndex].fields[itemIndex]
+    getPaletteItem: (_, getters) => (groupIndex, itemIndex) => {
+      return getters.paletteState.groups[groupIndex].fields[itemIndex]
+    }
+  },
+  mutations: {
+    createSingleton({paletteConfig: {createdSingletons}}, type) {
+      createdSingletons[type] = true
+    },
+    destroySingleton({paletteConfig: {createdSingletons}}, type) {
+      createdSingletons[type] = false
     }
   }
 }
