1c2ba060df95a887e70681798cd6e7a5d8119f49
Onur D <contact@dinony.com>
2018-01-08 12:11:39 +0100

    Add root wrapper div

--- src/components/builder/Builder.js
+++ src/components/builder/Builder.js
@@ -2,6 +2,10 @@ export default {
   functional: true,
   render(h, {props:{rootNode}}) {
     // Children of root node must be 'page' nodes
-    return rootNode.children.map(child => child.renderNode(h))
+    return (
+      <div id="root">
+        {rootNode.children.map(child => child.renderNode(h))}
+      </div>
+    )
   }
 }
