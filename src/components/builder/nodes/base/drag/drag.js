67be572060006e3180d2de615e45547a65928669
Onur D <contact@dinony.com>
2018-01-09 14:02:42 +0100

    Add scss compilation

--- src/components/builder/nodes/base/drag/drag.js
+++ src/components/builder/nodes/base/drag/drag.js
@@ -1,5 +1,7 @@
 import {WrapperNode} from '../wrapper'
 
+import './drag.scss'
+
 export const NODE_TYPE_DRAG = 'drag'
 
 export class DragNode extends WrapperNode {
@@ -10,7 +12,7 @@ export class DragNode extends WrapperNode {
 
   renderNode(h) {
     return (
-      <div>
+      <div class="drag-node">
         <div>Drag handler</div>
         <div>Edit Btn</div>
         {this.child.renderNode(h)}
