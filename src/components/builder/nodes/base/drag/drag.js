0d86fd7335144d8261b9ce10e9a632741a707447
Onur D <contact@dinony.com>
2018-01-27 17:58:26 +0100

    Adapt drag wrapper

--- src/components/builder/nodes/base/drag/drag.js
+++ src/components/builder/nodes/base/drag/drag.js
@@ -1,5 +1,5 @@
 import faArrowsAlt from '@fortawesome/fontawesome-free-solid/faArrowsAlt'
-
+import {DnDHandle} from 'mo-vue-dnd'
 import {IntermediateNode} from '../intermediate'
 
 import './drag.scss'
@@ -10,14 +10,31 @@ export class DragNode extends IntermediateNode {
   constructor(child) {
     super([child])
     this.type = NODE_TYPE_DRAG
+    this.hover = false
   }
 
-  renderNode(h) {
+  mouseenterHandler(event) {
+    this.hover = true
+  }
+
+  mouseleaveHandler(event) {
+    this.hover = false
+  }
+
+  renderNode(h, payload) {
+    const classSettings = {
+      'drag-node': true,
+      'drag-node--hover-state': this.hover
+    }
     return (
-      <div class="drag-node">
-        <div class="drag-handle hover-handle">
-          <fa-icon icon={faArrowsAlt}/>
-        </div>
+      <div class={classSettings}
+        onMouseenter={this.mouseenterHandler.bind(this)}
+        onMouseleave={this.mouseleaveHandler.bind(this)}>
+        <DnDHandle container={payload}>
+          <div class="drag-handle hover-handle">
+            <fa-icon icon={faArrowsAlt}/>
+          </div>
+        </DnDHandle>
         <div class="edit-btn hover-handle">Edit</div>
         <div class="child-wrapper">{this.children[0].renderNode(h)}</div>
       </div>)
