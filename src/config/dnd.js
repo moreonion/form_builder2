e703bef18c880bf46743ad016b16da6bb7f3c2d3
Onur D <contact@dinony.com>
2018-01-27 14:34:39 +0100

    Prep builder dnd options

--- src/config/dnd.js
+++ src/config/dnd.js
@@ -1,7 +1,13 @@
+import {DnDOptions} from 'mo-vue-dnd'
+
 export const PALETTE_DND_GROUP = 'palette'
 export const BUILDER_DND_GROUP = 'tree'
 
-export const BUILDER_DND_OPTIONS = {
-  group: {name: BUILDER_DND_GROUP, pull: true, put: [BUILDER_DND_GROUP, PALETTE_DND_GROUP]},
-  animation: 100
-}
+const builderOptions = new DnDOptions(true, false)
+// Just allow dnd on itself (nested)
+builderOptions.permissions = [
+  [BUILDER_DND_GROUP],
+  [BUILDER_DND_GROUP]
+]
+
+export const BUILDER_DND_OPTIONS = builderOptions
