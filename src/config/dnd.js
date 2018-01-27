e5ae643e6175ff5b97e0ec1c2e2b43e375cf090a
Onur D <contact@dinony.com>
2018-01-27 17:20:05 +0100

    Adapt builder options

--- src/config/dnd.js
+++ src/config/dnd.js
@@ -1,13 +1,17 @@
 import {DnDOptions} from 'mo-vue-dnd'
+import { builderModule } from '../components/builder/store';
 
 export const PALETTE_DND_GROUP = 'palette'
 export const BUILDER_DND_GROUP = 'tree'
 
-const builderOptions = new DnDOptions(true, false)
+const builderOptions = new DnDOptions(true, true)
 // Just allow dnd on itself (nested)
 builderOptions.permissions = [
   [BUILDER_DND_GROUP],
   [BUILDER_DND_GROUP]
 ]
+
+// Do not clone nodes, just pass reference
+builderOptions.cloneItemFn = node => node
 
 export const BUILDER_DND_OPTIONS = builderOptions
