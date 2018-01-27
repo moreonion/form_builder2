2bc8229ddef616ab44754d4b635470340bb190e0
Onur D <contact@dinony.com>
2018-01-27 14:50:48 +0100

    Rm key on page node. Cleanup fmt.

--- src/components/builder/nodes/general/page.js
+++ src/components/builder/nodes/general/page.js
@@ -6,16 +6,13 @@ export class PageNode extends IntermediateNode {
   constructor(initChildren=[], dndOptions=undefined) {
     super(initChildren, dndOptions)
     this.type = NODE_TYPE_PAGE
-    this.key = Math.random()
   }
 
   renderNode(h) {
     return (
-      <div key={this.key} id={this.path}>
+      <div id={this.path}>
         <h1>Page</h1>
-        <div>
-          {this.children.map(child => child.renderNode(h))}
-        </div>
+        <div>{this.children.map(child => child.renderNode(h))}</div>
       </div>)
   }
 }
