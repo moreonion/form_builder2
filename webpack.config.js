5c129e836e121a56bd838af5f7d1c92ad731c3d5
Onur D <contact@dinony.com>
2018-01-29 13:36:15 +0100

    Rm vue loader. Add postcss loader.

--- webpack.config.js
+++ webpack.config.js
@@ -11,26 +11,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {importLoaders: 2}},
+          {loader: 'postcss-loader'}
+        ]
       },
       {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader', options: {importLoaders: 2}},
+          {loader: 'postcss-loader'},
+          {loader: 'sass-loader'},
+        ]
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
