16598e09fcba6edfe69c9f613b007a1253f9077b
Onur D <contact@dinony.com>
2018-01-29 16:50:00 +0100

    Adapt clone method

--- src/config/dnd.js
+++ src/config/dnd.js
@@ -4,34 +4,34 @@ import { builderModule } from '../components/builder/store';
 export const PALETTE_DND_GROUP = 'palette'
 export const BUILDER_DND_GROUP = 'tree'
 
-// Do not clone nodes etc., just pass reference
-const passRef = obj => obj
-
 const palettePermissions = [
   [],                   // in: from nowhere
   [BUILDER_DND_GROUP]   // out: just to builder
 ]
 
+const createBuilderNode = item => item.nodeFactory()
+
 const contactItemsOptions = new DnDOptions()
 contactItemsOptions.permissions = palettePermissions
-contactItemsOptions.cloneItemFn = passRef
+contactItemsOptions.cloneItemFn = createBuilderNode
 
 export const PALETTE_CONTACT_DND_OPTIONS = contactItemsOptions
 
 const generalItemsOptions = new DnDOptions()
 generalItemsOptions.allowItemRemoval = false
 generalItemsOptions.permissions = palettePermissions
-generalItemsOptions.cloneItemFn = passRef
+generalItemsOptions.cloneItemFn = createBuilderNode
 
 export const PALETTE_GENERAL_DND_OPTIONS = generalItemsOptions
 
 const builderOptions = new DnDOptions(true, true)
 // Just allow dnd on itself (nested)
 builderOptions.permissions = [
-  [BUILDER_DND_GROUP],
-  [BUILDER_DND_GROUP]
+  [PALETTE_DND_GROUP, BUILDER_DND_GROUP], // in: from palette or builder
+  [BUILDER_DND_GROUP]                     // out: just to builder
 ]
 
-builderOptions.cloneItemFn = passRef
+// Do not clone nodes, just pass reference
+builderOptions.cloneItemFn = node => node
 
 export const BUILDER_DND_OPTIONS = builderOptions
