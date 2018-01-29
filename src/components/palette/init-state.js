94281a16cdaa08cccfd71e27fcf5445c24461356
Onur D <contact@dinony.com>
2018-01-29 16:21:50 +0100

    Mv dnd options init

--- src/components/palette/init-state.js
+++ src/components/palette/init-state.js
@@ -1,34 +1,21 @@
-import {DnDOptions} from 'mo-vue-dnd'
 import {Palette, PaletteGroup} from './palette-model'
 
-import TextFieldItem from './fields/general/text'
-import PageFieldItem from './fields/general/page'
-import FieldSetItem from './fields/general/field-set'
+import TextFieldItem from './items/general/text'
+import PageFieldItem from './items/general/page'
+import FieldSetItem from './items/general/field-set'
 
-import FirstNameItem from './fields/contact/first-name'
-import LastNameItem from './fields/contact/last-name'
+import FirstNameItem from './items/contact/first-name'
+import LastNameItem from './items/contact/last-name'
 
-import {PALETTE_DND_GROUP, PALETTE_DND_OPTIONS, BUILDER_DND_GROUP} from '../../config/dnd'
-
-const palettePermissions = [
-  [],                   // in: from nowhere
-  [BUILDER_DND_GROUP]   // out: just to builder
-]
-
-const contactItemsOptions = new DnDOptions()
-contactItemsOptions.permissions = palettePermissions
-
-const generalItemsOptions = new DnDOptions()
-generalItemsOptions.allowItemRemoval = false
-generalItemsOptions.permissions = palettePermissions
+import {PALETTE_CONTACT_DND_OPTIONS, PALETTE_GENERAL_DND_OPTIONS} from '../../config/dnd'
 
 export function getInitPaletteState() {
   return new Palette([
-    new PaletteGroup('Contact', contactItemsOptions, [
+    new PaletteGroup('Contact', PALETTE_CONTACT_DND_OPTIONS, [
       new FirstNameItem(),
       new LastNameItem()
     ]),
-    new PaletteGroup('General fields', generalItemsOptions, [
+    new PaletteGroup('General fields', PALETTE_GENERAL_DND_OPTIONS, [
       new TextFieldItem(),
       new PageFieldItem(),
       new FieldSetItem()
--- src/config/dnd.js
+++ src/config/dnd.js
@@ -4,6 +4,27 @@ import { builderModule } from '../components/builder/store';
 export const PALETTE_DND_GROUP = 'palette'
 export const BUILDER_DND_GROUP = 'tree'
 
+// Do not clone nodes etc., just pass reference
+const passRef = obj => obj
+
+const palettePermissions = [
+  [],                   // in: from nowhere
+  [BUILDER_DND_GROUP]   // out: just to builder
+]
+
+const contactItemsOptions = new DnDOptions()
+contactItemsOptions.permissions = palettePermissions
+contactItemsOptions.cloneItemFn = passRef
+
+export const PALETTE_CONTACT_DND_OPTIONS = contactItemsOptions
+
+const generalItemsOptions = new DnDOptions()
+generalItemsOptions.allowItemRemoval = false
+generalItemsOptions.permissions = palettePermissions
+generalItemsOptions.cloneItemFn = passRef
+
+export const PALETTE_GENERAL_DND_OPTIONS = generalItemsOptions
+
 const builderOptions = new DnDOptions(true, true)
 // Just allow dnd on itself (nested)
 builderOptions.permissions = [
@@ -11,7 +32,6 @@ builderOptions.permissions = [
   [BUILDER_DND_GROUP]
 ]
 
-// Do not clone nodes, just pass reference
-builderOptions.cloneItemFn = node => node
+builderOptions.cloneItemFn = passRef
 
 export const BUILDER_DND_OPTIONS = builderOptions
