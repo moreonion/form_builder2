bf060d3faa568769aa39ad75ffab78af31b5bb78
Onur D <contact@dinony.com>
2018-02-05 16:41:10 +0100

    Rm unnecessary stuff

--- src/components/palette/Palette.js
+++ src/components/palette/Palette.js
@@ -1,48 +1,15 @@
 import {mapState, mapGetters} from 'vuex'
 import {DnDItems} from 'mo-vue-dnd'
 import './Palette.scss'
-import {encodePaletteItem, decodePaletteItem} from './util'
-import {PALETTE_DND_WRAPER_CLASSNAME} from '../../config/palette'
-import {BUILDER_ROOT_DIV_ID} from '../../config/builder'
-import PageFieldItem from './items/general/page'
 import {PALETTE_DND_GROUP} from '../../config/dnd'
 
 export default {
   components: {DnDItems},
   computed: {
-    paletteWrapperClsName: () => PALETTE_DND_WRAPER_CLASSNAME,
     ...mapState('palette', ['activeNames']),
     ...mapGetters('palette', {
       palette: 'paletteState'
     })
-  },
-  methods: {
-    encodePaletteItem,
-    moveHandler(event) {
-      const paletteModelId = decodePaletteItem(event.dragged.id)
-      if(paletteModelId !== null) {
-        const {paletteGroupIndex, paletteItemIndex} = paletteModelId
-        const paletteModel = this.$store.getters['palette/getPaletteItem'](paletteGroupIndex, paletteItemIndex)
-
-        if(paletteModel instanceof PageFieldItem) {
-          /*
-          * RULE: Page node is only allowed as child of root node.
-          */
-          return event.to.parentNode.id === BUILDER_ROOT_DIV_ID
-        } else if(event.to.parentNode.id === BUILDER_ROOT_DIV_ID) {
-          /*
-          * RULE: Children of root may only be page nodes.
-          */
-          return paletteModel instanceof PageFieldItem
-        }
-      }
-
-      return true
-    },
-    removeHandler(event) {
-    },
-    endHandler(event) {
-    }
   },
   render(h) {
     const slots = {default: props => props.item.renderFn(h, props.item)}
