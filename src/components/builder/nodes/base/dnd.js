120e42ca9c709a2c3468fdd4900b0576319b684a
Onur D <contact@dinony.com>
2018-02-05 16:42:40 +0100

    Rm unnecessary stuff

--- src/components/builder/nodes/base/dnd.js
+++ src/components/builder/nodes/base/dnd.js
@@ -1,12 +1,6 @@
 import {DnDItems} from 'mo-vue-dnd'
 import {IntermediateNode} from './intermediate'
-import {decodePaletteItem} from '../../../palette/util'
 import {BUILDER_DND_OPTIONS, BUILDER_DND_GROUP} from '../../../../config/dnd'
-import {PALETTE_DND_WRAPER_CLASSNAME} from '../../../../config/palette'
-import {BUILDER_ROOT_DIV_ID, BUILDER_IS_SINGLETON_NODE_MAP} from '../../../../config/builder'
-import {store} from '../../../../store'
-import {PageNode} from '../general/page'
-import {AbstractNode} from '../base/abstract'
 
 export const NODE_TYPE_DND = 'dnd'
 
