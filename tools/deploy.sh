89b4a84fbf8872f9ae215500d02a0ad0b7b240ca
Onur D <contact@dinony.com>
2018-02-13 21:02:10 +0100

    Adapt deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+# Cleanup
+ssh dinony@dinony.com 'cd www/static/projects/fb2; rm -rf dist'
+ssh dinony@dinony.com 'cd www/static/projects/fb2; ls'
+# Deploy
 scp -r dist dinony@dinony.com:~/www/static/projects/fb2/dist
 scp index.html dinony@dinony.com:~/www/static/projects/fb2/index.html
