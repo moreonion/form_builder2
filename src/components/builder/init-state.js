a307f8b331c9cd9acf6dab448ca32b5a81fc4864
Onur D <contact@dinony.com>
2018-02-13 23:14:47 +0100

    Adapt init state

--- src/components/builder/init-state.js
+++ src/components/builder/init-state.js
@@ -1,5 +1,6 @@
 import {IntermediateNode} from './nodes/base/intermediate'
 import {DnDNode} from './nodes/base/dnd'
+// import {DragNode} from './nodes/base/drag/drag'
 import {PageNode} from './nodes/general/page'
 import {TextFieldNode} from './nodes/general/text'
 import {FieldSetNode} from './nodes/general/field-set';
@@ -8,11 +9,14 @@ const initState = new IntermediateNode([
   new PageNode([
     new DnDNode([
       new FieldSetNode([
-        new DnDNode([
-          new TextFieldNode('Other')
-        ])
-      ]),
-      new TextFieldNode('Text 123'),
+        new DnDNode([])
+      ])
+      // new TextFieldNode('Text 123'),
+      // new FieldSetNode([
+      //    new DnDNode([
+      //      new TextFieldNode('text', 'label')
+      //    ])
+      // ])
     ])
   ])
 ])
