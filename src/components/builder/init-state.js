42ce01a7b99f28bcd6422a3a263149afcda15689
Onur D <contact@dinony.com>
2018-02-19 13:09:56 +0100

    Simplify init state

--- src/components/builder/init-state.js
+++ src/components/builder/init-state.js
@@ -1,23 +1,13 @@
 import {IntermediateNode} from './nodes/base/intermediate'
 import {DnDNode} from './nodes/base/dnd'
-// import {DragNode} from './nodes/base/drag/drag'
+import {DragNode} from './nodes/base/drag/drag'
 import {PageNode} from './nodes/general/page'
 import {TextFieldNode} from './nodes/general/text'
 import {FieldSetNode} from './nodes/general/field-set';
 
 const initState = new IntermediateNode([
-  new PageNode([
-    new DnDNode([
-      new FieldSetNode([
-        new DnDNode([])
-      ])
-      // new TextFieldNode('Text 123'),
-      // new FieldSetNode([
-      //    new DnDNode([
-      //      new TextFieldNode('text', 'label')
-      //    ])
-      // ])
-    ])
+  new DnDNode([
+    new DragNode(new TextFieldNode())
   ])
 ])
 
