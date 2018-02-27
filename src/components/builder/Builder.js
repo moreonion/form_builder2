980b844f2212a37911b016e01b4e96616e8d6e4d
Onur D <contact@dinony.com>
2018-02-27 12:59:09 +0100

    Prep builder and builder node communication

--- src/components/builder/Builder.js
+++ src/components/builder/Builder.js
@@ -1,13 +1,47 @@
+import bus from '../../bus'
+import {IntermediateNode} from './nodes/base/intermediate'
+import {
+  BUILDER_REFS_REQ, BUILDER_REFS_REQD,
+  BUILDER_BEFOREMOUNT, BUILDER_NODE_TRACED,
+  RefsRequestedPayload
+} from './events'
+import {doc, findAncestorByClassName} from '../../dom'
+
 export default {
-  functional: true,
-  render(h, {props:{rootNode}}) {
+  props: {
+    rootNode: {
+      type: IntermediateNode,
+      required: false
+    }
+  },
+  beforeMount() {
+    doc.addEventListener('mousemove', this.onMousemove)
+    bus.$on(BUILDER_REFS_REQ, this.onReqRefs)
+
+    bus.$emit(BUILDER_BEFOREMOUNT)
+  },
+  beforeDestroy() {
+    doc.removeEventListener('mousemove', this.onMousemove)
+    bus.$off(BUILDER_REFS_REQ, this.onReqRefs)
+  },
+  methods: {
+    onReqRefs(reqPayload) {
+      bus.$emit(BUILDER_REFS_REQD, new RefsRequestedPayload(reqPayload.node, this.$refs))
+    },
+    onMousemove(event) {
+      const elemAtPoint = doc.elementFromPoint(event.clientX, event.clientY)
+      const dragNode = findAncestorByClassName(elemAtPoint, 'drag-node')
+      bus.$emit(BUILDER_NODE_TRACED, dragNode)
+    }
+  },
+  render(h) {
     /* All model checks must be applied before. Builder
      * assumes valid model.
      * E.g. Children of root node must be 'page' nodes
      */
     return (
-      <div style={{'margin-left': '32px'}}>
-        {rootNode.children.map(child => child.renderFn(h))}
+      <div style="margin-left: 32px">
+        {this.rootNode.children.map(child => child.renderFn(h))}
       </div>)
   }
 }
